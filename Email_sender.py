5a694771b27894edebd3c84d3848901daa5a38f3
Loubens D <158304923+DLoubens@users.noreply.github.com>
2024-03-03 19:29:39 -0500

    Update Email_sender.py

--- Email_sender.py
+++ Email_sender.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 
 email = EmailMessage()
 email['from'] = 'LD Python'
-email['to'] = 'ing.d.loubens@gmail.com'
+email['to'] = 'xxxxxxxxx@gmail.com'
 email['subject'] = 'Test Message pythons'
 
 
@@ -12,6 +12,6 @@ email.set_content('I am Python programmer What is the next step')
 with smtplib.SMTP(host='smtp.gmail.com', port = 587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('kettia.tilor@gmail.com','ojchgfthxnmrminw')
+    smtp.login('yyyyyyyyyy@gmail.com','ojchgfthxnmrminw')
     smtp.send_message(email)
     print('all done!')
