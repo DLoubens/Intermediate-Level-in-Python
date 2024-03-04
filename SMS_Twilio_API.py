26ca9718c649cf875ce9d22061d6edbe140be9d0
Loubens D <158304923+DLoubens@users.noreply.github.com>
2024-03-03 19:25:30 -0500

    Update SMS_Twilio_API.py

--- SMS_Twilio_API.py
+++ SMS_Twilio_API.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = 'AC556c0fe8735dc8042185481deb46058f'
+account_sid = 'AC556c0fe8735dc8042185481de8f'
 auth_token = '[AuthToken]'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
   from='+18442532975',
   body='Thanks Twilio for the MSN',
-  to='+14704031009'
+  to='+14704031119'
 )
 
 print(message.sid)
